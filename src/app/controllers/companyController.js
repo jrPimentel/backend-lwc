f426a0bf04fc96480208af96cef97d33e8499384
Jhonata Pimentel <jhonatapimentel@outlook.com.br>
2019-10-31 10:43:26 -0300

    add company

--- src/app/controllers/companyController.js
+++ src/app/controllers/companyController.js
@@ -13,6 +13,7 @@ router.use(authMiddleware);
     const listCompanies = await Company.find().sort("-createdAt");
     return res.json(listCompanies);
   }),
+  
   //Selecionar uma compania. 
   router.get('/:companyId', async (req, res) => {
     const { name } = req.body;
@@ -21,21 +22,24 @@ router.use(authMiddleware);
   }),
   //add company 
   router.post('/company/add', async (req, res) => {
-    //const { _id } = req.body;
-    try {
-
-      if (await Company.findOne({ _id })) return res.status(400).send({ error: 'Company already exists' });
-
+  
+    const { email } = req.body;
+    
+    try{
+  
+      if(await Company.findOne({ email })) return res.status(400).send({ error: 'Company already exists'});
+  
       const company = await Company.create(req.body);
-
-
-      return res.send({ company });
-
-    } catch (err) {
-      return res.status(400).send({ error: 'Registration failed' })
-
+  
+      company.password = undefined;
+  
+  
+      return res.send({ company,  token: generateToken({ id: company.id }), });
+    } catch (err){
+      return res.status(400).send({ error: 'Registration failed'})
+      
     }
-
+  
   });
 //update company
   router.put('/:companyId', async (req, res) => {
