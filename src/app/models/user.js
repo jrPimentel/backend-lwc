ab2a024eb40f2524de7ea32e59820df753d01443
Jhonata Pimentel <jhonatapimentel@outlook.com.br>
2019-11-01 20:20:27 -0300

    Tipos de usuarios no banco.

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -2,48 +2,57 @@ const mongoose = require('../../database');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-
+  //guarda o nome. 
   name: { 
     type: String,
     required: true,
   },
+  //guarda o email 
   email: {
     type: String,
     unique: true,
     required: true,
     lowercase: true,
   },
+  //guarda a senha.
   password: {
     type: String,
     required: true,
     select: false,
   },
+  //token de reset pass. 
   passwordResetToken:{
     type: String,
     select: false,
   },
+  //tempo de espiração do reset pass.
   passwordResetExpires:{
     type: Date,
     select: false,
   },
+  //Atribui uma compania para o usuario.
   company:{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Company',
     required: true,
   },
+  //Para tornar o user root.
+  acctype: {
+    type: Boolean,
+    default: false,
+    select: false,
+  },
+  //data de criação de conta. 
   createdAt: {
     type: Date,
     default: Date.now,
   },
-
 })
-
 UserSchema.pre('save', async function(next){
       const hash = await bcrypt.hash(this.password, 10);
       this.password = hash;
       next();
 });
-
 const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
