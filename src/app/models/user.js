f17d182b753e040fe9d04886daa0369da2e49797
Matheus Betim <ma.betim@hotmail.com>
2019-11-05 07:57:39 -0300

    Fixing login as root

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,29 +1,29 @@
-const mongoose = require('../../database');
-const bcrypt = require('bcryptjs');
+const mongoose = require("../../database");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
-  //guarda o nome. 
+  //guarda o nome.
   name: { type: String, required: true },
-  //guarda o email 
+  //guarda o email
   email: { type: String, unique: true, required: true, lowercase: true },
   //guarda a senha.
   password: { type: String, required: true, select: false },
-  //token de reset pass. 
-  passwordResetToken:{ type: String, select: false },
+  //token de reset pass.
+  passwordResetToken: { type: String, select: false },
   //tempo de espiração do reset pass.
-  passwordResetExpires:{ type: Date, select: false },
+  passwordResetExpires: { type: Date, select: false },
   //atribui empresa para usuário.
-  company:{ type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true },
+  company: { type: mongoose.Schema.Types.ObjectId, ref: "Company", required: true },
   //Para tornar o user root.
-  accRoot: { type: Boolean, default: false, select: false },
-  //data de criação de conta. 
-  createdAt: { type: Date, default: Date.now },
-})
-UserSchema.pre('save', async function(next){
-      const hash = await bcrypt.hash(this.password, 10);
-      this.password = hash;
-      next();
+  accRoot: { type: Boolean, default: false, select: false, required: true },
+  //data de criação de conta.
+  createdAt: { type: Date, default: Date.now }
 });
-const User = mongoose.model('User', UserSchema);
+UserSchema.pre("save", async function(next) {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  next();
+});
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
