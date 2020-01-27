37c317894ad0cbee033ed3b57bdb18113b0dbdc6
mbetim <ma.betim@hotmail.com>
2020-01-27 00:01:50 -0300

    Update and delete company

--- src/app/controllers/companyController.js
+++ src/app/controllers/companyController.js
@@ -39,10 +39,43 @@ router.post("/add", async (req, res) => {
 
 //update company
 router.put("/:companyId", async (req, res) => {
-  res.send({ ok: true, company: req.companyId });
+  const { companyId } = req.params;
+
+  try {
+    if (await Company.find({ _id: companyId })) {
+      // const company = await Company.findOneAndUpdate({ _id: companyId }, req.body, {
+      //   returnNewDocument: true
+      // });
+      await Company.updateOne({ _id: companyId }, req.body);
+      const company = await Company.find({ _id: companyId });
+      const companies = await Company.find().sort("name");
+
+      res.send({ success: true, company, companies });
+    } else {
+      res.status(400).send({ success: false, error: "Company not found" });
+    }
+  } catch (err) {
+    res.status(400).send({ success: false, error: "Error when updating company" });
+  }
 });
+
 //delete company
 router.delete("/:companyId", async (req, res) => {
+  const { companyId } = req.params;
+
+  try {
+    if (await Company.find({ _id: companyId })) {
+      await Company.findOneAndDelete({ _id: companyId });
+      const companies = await Company.find().sort("name");
+
+      return res.send({ success: true, companies });
+    } else {
+      return res.status(400).send({ success: false, error: "Company not found" });
+    }
+  } catch (err) {
+    return res.status(400).send({ success: false, error: "Error when deleting company" });
+  }
   res.send({ ok: true });
 });
+
 module.exports = app => app.use("/company", router);
